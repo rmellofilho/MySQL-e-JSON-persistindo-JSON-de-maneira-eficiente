d45a8bdbb8254da9764e657391f21f102b4b7595
Robeto Melo <rmello.filho@hotmail.com>
2024-01-11 15:20:07 -0300

    12 - JSON Contains e Search

---
+++ 02 - Usando JSON no MySQL/12 - JSON Contains e Search.sql
@@ -0,0 +1,14 @@
+SELECT Y FROM X;
+
+
+SELECT JSON_CONTAINS(Y, '"2293-3343"', "$.telefone") FROM X;
+SELECT * FROM X WHERE JSON_CONTAINS(Y, '"2293-3343"', "$.telefone") = 1;
+SELECT * FROM X WHERE JSON_EXTRACT(Y, "$.telefone") = '2293-3343';
+
+SELECT JSON_SEARCH(Y, "ONE", "2293-3343"), Y FROM X;
+
+INSERT INTO X VALUES ('{"nome":"Katia", "endereco":"Rua X numero Y", "telefone":"2293-3343", "telefone2":"2293-3343"}');
+SELECT * FROM X;
+
+SELECT JSON_SEARCH(Y, "ONE", "2293-3343"), Y FROM X;
+SELECT JSON_SEARCH(Y, "ALL", "2293-3343"), Y FROM X;
