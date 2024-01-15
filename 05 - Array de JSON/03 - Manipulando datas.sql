454dd4cb9968d9d892a031bd3e20f64854e9bd2f
Robeto Melo <rmello.filho@hotmail.com>
2024-01-15 17:29:39 -0300

    03 - Manipulando datas

---
+++ 05 - Array de JSON/03 - Manipulando datas.sql
@@ -0,0 +1,33 @@
+
+SELECT
+	JSON_EXTRACT(JSON,"$.Primeiro_Nome")
+	,JSON_EXTRACT(JSON,"$.Data_Nascimento")
+    ,JSON_EXTRACT(JSON,"$.Salario")
+    FROM tb_object_funcionario;
+    
+    
+SELECT
+	REPLACE(JSON_EXTRACT(JSON,"$.Primeiro_Nome"),'"','')
+	,JSON_EXTRACT(JSON,"$.Data_Nascimento")
+    ,JSON_EXTRACT(JSON,"$.Salario")
+    FROM tb_object_funcionario;
+    
+SELECT
+	JSON_UNQUOTE(JSON_EXTRACT(JSON,"$.Primeiro_Nome"))
+	,JSON_UNQUOTE(JSON_EXTRACT(JSON,"$.Data_Nascimento"))
+    ,JSON_UNQUOTE(JSON_EXTRACT(JSON,"$.Salario"))
+    FROM tb_object_funcionario;
+    
+SELECT
+	JSON_TYPE(JSON_EXTRACT(JSON,"$.Primeiro_Nome"))
+	,JSON_TYPE(JSON_EXTRACT(JSON,"$.Data_Nascimento"))
+    ,JSON_TYPE(JSON_EXTRACT(JSON,"$.Salario"))
+    FROM tb_object_funcionario;
+    
+    
+SELECT
+	JSON_UNQUOTE(JSON_EXTRACT(JSON,"$.Primeiro_Nome"))
+	,JSON_UNQUOTE(JSON_EXTRACT(JSON,"$.Data_Nascimento"))
+    ,JSON_EXTRACT(JSON,"$.Salario")
+    FROM tb_object_funcionario;
+    
